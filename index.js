86dcd7d0223d2737e6131406c2604a74a1341846
ramalingaa <87629304+ramalingaa@users.noreply.github.com>
2021-09-01 14:37:42 +0530

    feat:adding features to main

--- index.js
+++ index.js
@@ -2,16 +2,18 @@ var billAmount =document.querySelector("#bill-amount");
 var cashGiven = document.querySelector("#Cash-given");
 var btnClick = document.querySelector("#btn-click");
 var table = document.getElementById("table");
+var failedMess = document.querySelector("#falied-mess")
 // var noOfNotes = document.querySelector(".noOf-Notes");
 
 function clickHandler(){
-    var billAmountIn = billAmount.value;
-    var cashGivenIn = cashGiven.value;
+    var billAmountIn = Number(billAmount.value);
+    var cashGivenIn = Number(cashGiven.value);
     
     var returnChange = [];
     
     if(cashGivenIn > billAmountIn){
-    let diffAmount = cashGivenIn - billAmountIn;
+        if(billAmountIn > 0){
+       let diffAmount = cashGivenIn - billAmountIn;
        let  denomoe =[2000,500,200,100,50,20,10,5,1];
       
        for (let notes of denomoe){
@@ -21,17 +23,30 @@ function clickHandler(){
            diffAmount = diffAmount % notes;
 
        }
+       hideMessage();
+       for(var i = 0; i < (table.rows.length-1); i++)
+       {
+         // cells
+         for(var j = 1; j < table.rows[i].cells.length; j++)
+         {
+             table.rows[i].cells[j].innerText = returnChange[j-1];
+         }
+       }
        
+    }}
+    else{
+         showMessage()
     }
-    // for(var i = 0; i < (table.rows.length-1); i++)
-    //     {
-    //       // cells
-    //       for(var j = 1; j < table.rows[i].cells.length; j++)
-    //       {
-    //           table.rows[i].cells[j].innerText = returnChange[j-1];
-    //       }
-    //     }
-    console.log(returnChange)
+    function hideMessage() {
+        failedMess.style.display = "none";
+      }
+      
+      function showMessage(msg) {
+        failedMess.style.display = "block";
+        const newString = "Do not come Tomorrow";
+        failedMess.innerText = newString;
+        }
+      
       
         
 }
